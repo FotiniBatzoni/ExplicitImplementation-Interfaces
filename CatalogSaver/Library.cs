7b294d74908a917e63f75c8b1f738c336e761aa7
fotini <f.batzoni@gmail.com>
2023-03-17 17:50:18 +0200

    Add project files.

---
+++ CatalogSaver/Library.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CatalogSaver
+{
+    public interface ISaveable
+    {
+        void Save();
+    }
+
+    public class Catalog : ISaveable
+    {
+        //when we use explicit implementation 
+        //we are not allowed to put access modifiers on the members
+        void ISaveable.Save()
+        {
+            Console.WriteLine("Saved");
+        }
+    }
+}
---
+++ CatalogSaver/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CatalogSaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalog catalog = new Catalog();
+            //catalog.Save();   //compiler error
+
+            ISaveable saveable = new Catalog();
+            saveable.Save();
+
+            ((ISaveable)catalog).Save();
+
+            var varCatalog = new Catalog();
+            // varCatalog.Save();   //compiler error
+
+
+        }
+    }
+}
